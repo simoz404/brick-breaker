f762ba3a3ac68f0f06a41730a3df364b34b64815
saljaoui <soufian2022ff@gmail.com>
2024-12-25 09:55:53 +0100

    add my files from soufian

---
+++ js/moveBall.js
@@ -0,0 +1,24 @@
+let ball = document.getElementById('ball')
+
+let velocityX = -3
+let velocityY = -3
+let ballX = 400;
+let ballY = 500;
+
+function moveBall() {
+    ballX += velocityX;
+    ballY += velocityY;
+
+    if (ballX < 0 || ballX > 720) {
+        velocityX *= -1;
+    }
+    if (ballY < 0 || ballY > 550) {
+        velocityY *= -1;
+    }
+
+    ball.style.transform = `translate(${ballX}px, ${ballY}px)`;
+
+    requestAnimationFrame(moveBall);
+}
+
+requestAnimationFrame(moveBall)

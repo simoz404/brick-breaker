8184b45b50fdff19eb5d8726ada511d259243c6d
saljaoui <soufian2022ff@gmail.com>
2024-12-25 16:35:39 +0100

    fix paddle

--- js/moveBall.js
+++ js/moveBall.js
@@ -1,37 +1,39 @@
-let ball = document.getElementById('ball')
-let velocityX = -2
-let velocityY = -2
+let ball = document.getElementById("ball");
+
+let velocityX = -2;
+let velocityY = -2;
 let ballX = 400;
 let ballY = 500;
-let isrecersived = false
+let isrecersived = false;
 
 function moveBall() {
-    let recPaddle = paddle.getBoundingClientRect()
-    let recBall = ball.getBoundingClientRect()
-    ballX += velocityX;
-    ballY += velocityY;
+  let recPaddle = paddle.getBoundingClientRect();
+  let recBall = ball.getBoundingClientRect();
 
-    if (ballX < 0 || ballX > 720) {
-        velocityX *= -1;
-    }
+  ballX += velocityX;
+  ballY += velocityY;
 
-    if (ballY < 0 || ((recBall.left <= recPaddle.right && recBall.right >= recPaddle.left) && (ballY === 530 && !isrecersived))) {
-        velocityY *= -1;
-    } else if (((recBall.left <= recPaddle.right && recBall.right >= recPaddle.left) && (ballY === 550))) {
-      velocityY *= -1;
-      velocityX *= -1;
-      isrecersived = true
-      setTimeout (() => {
-        isrecersived = false
-      }, 1000)
-    }
+  if (ballX < 0 || ballX > 720) {
+    velocityX *= -1;
+  }
 
-    
-    if (ballY > 600) {
-        alert("Game Over")
-        return
-    }
-    ball.style.transform = `translate(${ballX}px, ${ballY}px)`;
+  if ( ballY < 0 || (recBall.left <= recPaddle.right && recBall.right >= recPaddle.left && ballY === 530 && !isrecersived)) {
+    velocityY *= -1;
+  } else if ( recBall.left <= recPaddle.right && recBall.right >= recPaddle.left && ballY === 550) {
+    velocityY *= -1;
+    velocityX *= -1;
+    isrecersived = true;
+    setTimeout(() => {
+      isrecersived = false;
+    }, 1000);
+  }
+
+  if (ballY > 600) {
+    alert("Game Over");
+    return;
+  }
+  
+  ball.style.transform = `translate(${ballX}px, ${ballY}px)`;
 
   requestAnimationFrame(moveBall);
 }

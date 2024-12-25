37cf56f787ad0eae49fdfe9944cd6687eb459532
mbakhouc <bakhouch2019@gmail.com>
2024-12-25 16:36:33 +0100

    s

--- js/moveBall.js
+++ js/moveBall.js
@@ -1,3 +1,4 @@
+//import { bricksBreaker } from "./bricksBreaker.js";
 let ball = document.getElementById('ball')
 let velocityX = -3
 let velocityY = -3
@@ -5,24 +6,26 @@ let ballX = 400;
 let ballY = 500;
 
 function moveBall() {
-    let rec = paddle.getBoundingClientRect()
-    let rec2 = ball.getBoundingClientRect()
+    let recPaddle = paddle.getBoundingClientRect()
+    let recBall = ball.getBoundingClientRect()
     ballX += velocityX;
     ballY += velocityY;
 
     if (ballX < 0 || ballX > 720) {
         velocityX *= -1;
     }
-    
-    if (ballY < 0 || ((rec2.x <= rec.right && rec2.x >= rec.left) && (ballY > 530))) {
+    if (ballY < 0 || ((recBall.left <= recPaddle.right && recBall.right >= recPaddle.left) && (ballY === 530))) {
         velocityY *= -1;
+    } else if (((recBall.left <= recPaddle.right && recBall.right >= recPaddle.left) && (ballY === 550))) {
+      velocityY *= -1;
+      velocityX *= -1;
     }
     if (ballY > 600) {
         alert("Game Over")
         return
     }
     ball.style.transform = `translate(${ballX}px, ${ballY}px)`;
-
+    //bricksBreaker(ballRec.x, ballRec.y)
     requestAnimationFrame(moveBall);
 }
 

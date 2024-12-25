457b28fff5d05b5b55bd99c4cd03c189cc3f03b9
mbakhouc <bakhouch2019@gmail.com>
2024-12-25 11:38:34 +0100

    readu foir merge

--- js/ballMove.js
+++ js/ballMove.js
@@ -1,18 +1,43 @@
 let ball = document.getElementById('ball')
 
-let velocityX = -3
-let velocityY = -3
+let velocityX = 3
+let velocityY = 3
 
 function moveBall() {
 
     let newX = ball.offsetLeft + velocityX;
     let newY = ball.offsetTop + velocityY;
 
+    let paddle = document.getElementById("paddle");
+    var rect = paddle.getBoundingClientRect();
+    var rect2 = ball.getBoundingClientRect();
+
     if (newX < 10 || newX > 730) {
         velocityX *= -1;
-    } else if (newY < 10 || newY > 550) {
+    } else if (newY < 10 || ((rect.right < rect2.right || rect.left > rect2.left) && rect.top <= rect2.top+16.5)) {
         velocityY *= -1;
     }
+    //console.log(ball.offsetTop);
+    
+    // console.log(rect.top);
+    // console.log(rect2.top);
+    
+    // if ((rect.right >= rect2.right && rect.left <= rect2.left && rect.top <= rect2.top+16.5)) {
+    //     console.log("here");
+        
+    //     velocityY *= -1;
+        
+    // }
+    // console.log(rect.right, rect.left);
+    // console.log(rect2.right, rect2.left);
+
+    // if (newY >= paddle.offsetLeft && newY <= paddle.offsetLeft + 70) {
+
+    //     console.log('here');
+        
+    // }
+    
+    
 
     ball.style.left = newX + 'px';
     ball.style.top = newY + 'px';

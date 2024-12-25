f2f52577d9646b940398f4e8dff8a359f9f83bca
saljaoui <soufian2022ff@gmail.com>
2024-12-25 16:20:03 +0100

    fix paddle

--- js/moveBall.js
+++ js/moveBall.js
@@ -1,8 +1,9 @@
 let ball = document.getElementById('ball')
-let velocityX = -3
-let velocityY = -3
+let velocityX = -2
+let velocityY = -2
 let ballX = 400;
 let ballY = 500;
+let isrecersived = false
 
 function moveBall() {
     let recPaddle = paddle.getBoundingClientRect()
@@ -13,10 +14,18 @@ function moveBall() {
     if (ballX < 0 || ballX > 720) {
         velocityX *= -1;
     }
-    
-    if (ballY < 0 || ((recBall.left <= recPaddle.right && recBall.right >= recPaddle.left) && (ballY > 530))) {
+
+    if (ballY < 0 || ((recBall.left <= recPaddle.right && recBall.right >= recPaddle.left) && (ballY === 530 && !isrecersived))) {
         velocityY *= -1;
+    } else if (((recBall.left <= recPaddle.right && recBall.right >= recPaddle.left) && (ballY === 550))) {
+      velocityY *= -1;
+      velocityX *= -1;
+      isrecersived = true
+      setTimeout (() => {
+        isrecersived = false
+      }, 1000)
     }
+
     if (ballY > 600) {
         alert("Game Over")
         return

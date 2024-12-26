c1e062521beeb1d3891c8868c54b3282acaca3b0
saljaoui <soufian2022ff@gmail.com>
2024-12-26 17:54:08 +0100

    add ball movemenet

--- js/moveBall.js
+++ js/moveBall.js
@@ -1,83 +1,96 @@
+
+
 let ball = document.getElementById("ball");
 
 let bricks = document.getElementsByClassName("brick")
 
+let velocityX = -5;
+let velocityY = 5;
+
+
+let ballx = 400;
+let bally = 500;
+let div = document.querySelector("#game-area")
+
+
 function bricksBreakid() {
-  
+
   for (let brick of bricks) {
     let recBrick = brick.getBoundingClientRect();
     let recBall = ball.getBoundingClientRect();
-    
-    if ( !brick.classList.contains('breaked')  && (recBall.right > recBrick.left && 
-        recBall.left < recBrick.right &&
-        recBall.bottom > recBrick.top && 
-        recBall.top < recBrick.bottom)) {
+    if ( !brick.classList.contains('breaked') && (recBall.right >= recBrick.left && 
+      recBall.left <= recBrick.right &&
+      recBall.bottom >= recBrick.top && 
+      recBall.top <= recBrick.bottom) ) {
       brick.classList.add('breaked');
-      velocityY *= -1;
+          velocityY *= -1
     }
+   
   }
+  
 }
 
-
-let velocityX = -2;
-let velocityY = -2;
-let ballX = 400;
-let ballY = 500;
-let isrecersived = false;
 
 function moveBall() {
-  let recPaddle = paddle.getBoundingClientRect();
-  let recBall = ball.getBoundingClientRect();
+    ballx += velocityX;
+    bally += velocityY;
+    let ball = document.getElementById('ball')
+    let ballRect = ball.getBoundingClientRect()
 
-  ballX += velocityX;
-  ballY += velocityY;
-  if (ballX < 0 || ballX > 720) {
-    velocityX *= -1;
-  }
+    let paddle = document.getElementById('paddle')
+    let rec = paddle.getBoundingClientRect()
+    let game = div.getBoundingClientRect()
 
-  if ( ballY < 0 || (recBall.left <= recPaddle.right && recBall.right >= recPaddle.left && ballY === 530 && !isrecersived)) {
-    velocityY *= -1;
-  } else if ( recBall.left <= recPaddle.right && recBall.right >= recPaddle.left && ballY === 550) {
-    velocityY *= -1;
-    velocityX *= -1;
-    isrecersived = true;
-    setTimeout(() => {
-      isrecersived = false;
-    }, 1000);
-  }
 
-  if (ballY > 600) {
-    alert("Game Over");
-    return;
-  }
+    if (ballx <= 0 || ballx > game.width - ballRect.width) {
+        velocityX *= -1;
+    } else if (bally < 0) {
+        velocityY *= -1;
+    }
+    else if (ballRect.y + ballRect.height >= game.height) {
+        return alert("game over");
+    }
 
-  ball.style.transform = `translate(${ballX}px, ${ballY}px)`;
+    if (detecteted(ballRect, rec)) {
+        let hitPosition = postion(ballRect, rec); 
 
-  bricksBreakid()
-  requestAnimationFrame(moveBall);
+        if (hitPosition < -0.3) {
+            velocityX *= -1 
+        } 
+        else if (hitPosition > 0.3) {
+            velocityX *= 1 
+        } 
+        else {
+            velocityX *= 0.5; 
+        }
+        let angleEffect = hitPosition * Math.PI / 6;
+        let newSpeed = Math.sqrt(velocityX * velocityX + velocityY * velocityY);
+    
+        velocityX = newSpeed * Math.sin(angleEffect); 
+        velocityY = -Math.abs(newSpeed * Math.cos(angleEffect));
+    }
+    
+    ball.style.transform = `translate(${ballx}px, ${bally}px)`
+    bricksBreakid()
+    requestAnimationFrame(moveBall);
+
 }
 
 
 
+function detecteted(ballRect, rec) {
+    return ballRect.x + ballRect.width > rec.x &&
+        ballRect.x < rec.x + rec.width &&
+        ballRect.y + ballRect.height > rec.y && 
+        ballRect.y < rec.y + rec.height
+}
 
+function postion(ballRect, rec) {
+  let ballCenter = ballRect.x + ballRect.width / 2; // مركز الكرة
+  let paddleCenter = rec.x + rec.width / 2;         // مركز المضرب
+  let hitPosition = ballCenter - paddleCenter;
+  let direction = hitPosition / (rec.width / 2);
+  return direction
+}
 
-
-// function randomVelocities() {
-//     if (velocityX > 0) {
-//         velocityX += Math.random();
-//         velocityX = Math.min(Math.max(velocityX, 2), 3);
-//     } else {
-//         velocityX -= Math.random();
-//         velocityX = Math.max(Math.min(velocityX, -2), -3);
-//     }
-
-//     if (velocityY > 0) {
-//         velocityY += Math.random();
-//         velocityY = Math.min(Math.max(velocityY, 2), 3);
-//     } else {
-//         velocityY -= Math.random();
-//         velocityY = Math.max(Math.min(velocityY, -2), -3);
-//     }
-// }
-
-requestAnimationFrame(moveBall);
+moveBall()

7acf2b9e93ce58d18c44fcb21317c8ded17ea01f
omrharbi <rharbu.omar@gmail.com>
2024-12-31 11:41:25 +0100

    done

--- js/moveBall.js
+++ js/moveBall.js
@@ -5,28 +5,33 @@ let velocityX = -1;
 let velocityY = 2;
 let ballX = 400;
 let ballY = 500;
-let scorep = 0;
+let isrecersived = false;
+let scorep =0
 let div = document.querySelector("#game-area")
 let currentBrickIndex = 0;
 function bricksBreakid(ballRect) {
   for (let brick of bricks) {
     let brickRect = brick.getBoundingClientRect();
 
+
+
+    if (topdetected(ballRect, brickRect) && !brick.classList.contains('breaked')) {
+      let hitPosition = postion(ballRect, brickRect);
+      console.log(hitPosition, velocityY , velocityX);
+      
       if (hitPosition < -0.5) {
         velocityY *= -1;
       } else if (hitPosition > 0.5) {
         velocityY *= 1;
-      } else {
+      } else {        
         velocityY *= 0.5;
       }
-      if (brick.className.includes('crack')) {
-        brick.classList.add('breaked');
-      }
-      brick.classList.add('crack');
+      brick.classList.add('breaked');
       scorep++
       score()
       let angleEffect = hitPosition * Math.PI / 6;
       velocityX = 3 * Math.sin(angleEffect);
+
       if (ballRect.bottom >= brickRect.top && ballRect.top <= brickRect.top) {
         velocityY = -Math.abs(3 * Math.cos(angleEffect));
       } else if (ballRect.top <= brickRect.bottom && ballRect.bottom >= brickRect.bottom) {
@@ -34,7 +39,7 @@ function bricksBreakid(ballRect) {
       }
     }
   }
- 
+}
 
 
 function moveBall() {
@@ -45,22 +50,23 @@ function moveBall() {
   let rec = paddle.getBoundingClientRect()
   let game = div.getBoundingClientRect()
 
-  if (ballX <= 0) {
-    ballX = 0
-    velocityX *= -1;
-  } else if (ballX > game.width - ballRect.width) {
-    ballX = game.width - ballRect.width
-    velocityX *= -1;
-  }
+    if (ballX <= 0) {
+      ballX = 0
+      velocityX *= -1;
+    } else if (ballX > game.width - ballRect.width) {
+      ballX = game.width - ballRect.width
+      velocityX *= -1;
+    }
 
-  if (ballY <= 0) {
+   if (ballY <= 0) {
     velocityY *= -1;
   }
- 
+
   bricksBreakid(ballRect)
 
   if (topdetected(ballRect, rec)) {
-     let hitPosition = postion(ballRect, rec);
+    isrecersived = false
+    let hitPosition = postion(ballRect, rec);
     console.log(hitPosition);
     if (hitPosition < -0.3) {
       velocityX *= -1
@@ -71,6 +77,7 @@ function moveBall() {
     else {
       velocityX *= 0.5;
     }
+
     let angleEffect = hitPosition * Math.PI / 6;
     velocityX = (3 * Math.sin(angleEffect));
     velocityY = -Math.abs(3 * Math.cos(angleEffect));
@@ -93,7 +100,7 @@ function detecteted(ballRect, rec) {
     ballRect.y < rec.y + rec.height
 }
 function topdetected(ballRect, brick) {
-  return ballRect.right >= brick.left &&
+  return   ballRect.right >= brick.left &&
     ballRect.left <= brick.right &&
     ballRect.bottom >= brick.top &&
     ballRect.top <= brick.bottom
@@ -110,33 +117,33 @@ var t;
 function timer() {
   var sec = 1
   var min = 0
-  let timer = document.getElementById('timer')
-  let s = '0'
-  let m = '0'
-  t = setInterval(() => {
-    if (!isPause) {
-      timer.innerHTML = 'Timer: ' + m + min + ':' + s + sec;
-      if (sec >= 9) {
-        s = ''
-      }
-      if (min >= 9) {
-        m = ''
-      }
-      if (sec == 60) {
-        s = '0'
-        sec = -1
-        min++
-      }
-      sec++
+    let timer = document.getElementById('timer')
+    let s = '0'
+    let m = '0'
+    t = setInterval(() => {
+        if (!isPause) {
+        timer.innerHTML = 'Timer: '+m+min+':'+s+sec;
+        if (sec >= 9) {
+            s = ''
+        } 
+        if (min >= 9) {
+            m = ''
+        }
+        if (sec == 60) {
+            s = '0'
+            sec = -1
+            min++
+        }
+        sec++
     }
     }, 1000)
 }
 
 function stop() {
-  clearInterval(t)
+    clearInterval(t)
 }
 
 function score() {
-  let scorediv = document.getElementById("score")
-  scorediv.innerHTML = 'Score: ' + scorep
+    let scorediv = document.getElementById("score")
+    scorediv.innerHTML = 'Score: '+ scorep
 }
